94d1b44010dcc7b862df3fbd097a182afc894f59
contail <qkrtkdwls2121@gmail.com>
2019-03-06 12:54:30 +0900

    add ec2 instacne method

--- scripts/ServerInfo/servers.js
+++ scripts/ServerInfo/servers.js
@@ -1,31 +1,86 @@
 const AWS = require('aws-sdk');
 const Promise = require("bluebird");
-const credentials = new AWS.SharedIniFileCredentials({profile: 'your profile'});
+const credentials = new AWS.SharedIniFileCredentials({profile: 'whale'});
 AWS.config.credentials = credentials;
 
 class Server{
     constructor() {
-        this.ec2 = new AWS.EC2({region: 'your region (ex: ap-northeast-2)'});
+        this.ec2 = new AWS.EC2({region: 'ap-northeast-2'});
+        this.rds = new AWS.RDS({region: 'ap-northeast-2'});
         this.currentEc2Count = this.currentEc2Count.bind(this);
+        this.addEc2 = this.addEc2.bind(this);
       }
+    
+    addEc2(){
+        var params = {
+            ImageId: "ami-xxx", 
+            MaxCount : 1,
+            MinCount : 1,
+            KeyName: "xxx", 
+            InstanceType: "m4.xlarge", 
+            NetworkInterfaces: [{
+                        AssociatePublicIpAddress: true,
+                        DeleteOnTermination: true,
+                        DeviceIndex: 0,
+                        SubnetId: 'subnet-xxx',
+                        Groups: [
+                          'sg-xxx'
+                        ]
+                    }],
+        
+            TagSpecifications: [
+                {
+               ResourceType: "instance", 
+               Tags: [
+                  {
+                 Key: "xxx", 
+                 Value: "xxx"
+                },
+                {
+                    Key: "xxx", 
+                    Value: "xxx"
+                   }
+               ]
+              }
+             ]
+        
+        };
+        
+        this.ec2.runInstances.bind(this.ec2)(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else{
+                console.log(data);
+            }           // successful response
+            /*
+            data = {
+            }
+            */
+          });
+    }
 
     currentEc2Count() {
            
         return Promise.promisify(this.ec2.describeInstances.bind(this.ec2))({}).then((data) => {
             let currentCount = 0;
-            
+            let tempCount = 0;
             for (let reservation of Array.from(data.Reservations)) {
                 for (let instance of Array.from(reservation.Instances)) {
+            
                     for (let tag of Array.from(instance.Tags)) {
                         if ((tag.Value.includes("web")) && (tag.Key === "Name")) {
                             currentCount += 1;
                         } 
+                        else if((tag.Key === "new")) {
+                            tempCount += 1;
+                        }
                     }
                 }
             }
+    
             return new Promise((resolve, reject) => {
                 resolve({
                     currentCount: currentCount,
+                    tempCount : tempCount,
                 })
             })
         }).catch((err) => {
--- scripts/server.js
+++ scripts/server.js
@@ -1,13 +1,21 @@
 const _ = require('lodash');
 const Server = require('./ServerInfo/servers');
 module.exports = (robot) => {
-    robot.respond(/서버 몇[대개]야/i, res => {
+    robot.respond(/웹서버 몇[대개]야/i, res => {
         res.send("잠시만 기다려주세요~ 알아보는 중이에요!");
         const server = new Server();
         server.currentEc2Count().then((counts) => {
-          res.send(`총 ${counts.currentCount}대 입니다~`)
+          res.send(`총 ${counts.currentCount + counts.tempCount}대고, 임시로 늘린 서버는 ${counts.tempCount}대 입니다~`)
         }).catch((err) => {
           console.error(err);
         });
     });
-}     
+
+    robot.respond(/서버 늘려줘/i, res => {
+        res.send("웹서버를 1대 늘리는 중입니다!");
+        const server = new Server();
+        server.addEc2();
+    });
+
+    
+}  
